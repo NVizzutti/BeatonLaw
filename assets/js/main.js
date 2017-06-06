4a5c2cb4bce777faa3adc7e37da8b5cea5860aa5
Nick Vizzutti <njvizzutti@gmail.com>
2017-06-05 17:10:33 -0700

    menu animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
+$('#dropdown').on('mouseover', (e) => {
+  $('#menu-items').css('visibility', 'visible');
+  console.log(parseInt($('#menu-items')[0].style.left))
+  if ($('#menu-items')[0].style.left )
+  $('#menu-items').animate({left: "-=200"}, 1400)
+})
 
+$('#dropdown').on('mouseout', (e) => {
+  $('#menu-items').css('visibility', 'hidden')
+})
+
+$(document).ready(() => {
+  $('#navbar').animate({opacity: 1}, 1500)
+})
